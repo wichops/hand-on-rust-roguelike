fa6a3e0114d365ad3eb39a65f30bcc52794e8e63
Luis Argumedo <huicho.argumedo@gmail.com>
2022-06-14 20:48:14 -0500

    feat: collisions System, enemy Component

--- src/main.rs
+++ src/main.rs
@@ -37,9 +37,15 @@ impl State {
         let mut ecs = World::default();
         let mut resources = Resources::default();
         let mut rng = RandomNumberGenerator::new();
-
         let map_builder = MapBuilder::new(&mut rng);
+
         spawn_player(&mut ecs, map_builder.player_start);
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
 
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
