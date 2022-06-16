b09704eee881627717054672705e14105c7c1f3b
Luis Argumedo <huicho.argumedo@gmail.com>
2022-06-16 16:38:36 -0500

    feat: added hud and tooltips systems

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,15 +1,16 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
-#[write_component(Player)]
+#[read_component(Point)]
+#[read_component(Player)]
 pub fn player_input(
     ecs: &mut SubWorld,
-    #[resource] map: &Map,
+    commands: &mut CommandBuffer,
     #[resource] key: &Option<VirtualKeyCode>,
-    #[resource] camera: &mut Camera,
     #[resource] turn_state: &mut TurnState,
 ) {
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+
     if let Some(key) = key {
         let delta = match key {
             VirtualKeyCode::Up => Point::new(0, -1),
@@ -19,18 +20,17 @@ pub fn player_input(
             _ => Point::zero(),
         };
 
-        if delta.x != 0 || delta.y != 0 {
-            let mut players = <&mut Point>::query().filter(component::<Player>());
+        players.iter(ecs).for_each(|(entity, pos)| {
+            let destination = *pos + delta;
 
-            players.iter_mut(ecs).for_each(|pos| {
-                let destination = *pos + delta;
-
-                if map.can_enter_tile(destination) {
-                    *pos = destination;
-                    camera.on_player_move(destination);
-                    *turn_state = TurnState::PlayerTurn;
-                }
-            })
-        }
+            commands.push((
+                (),
+                WantsToMove {
+                    entity: *entity,
+                    destination,
+                },
+            ));
+            *turn_state = TurnState::PlayerTurn;
+        });
     }
 }
--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -1,12 +1,12 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(MoveRandomly)]
-pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
-    let mut movers = <(&mut Point, &MoveRandomly)>::query();
+pub fn random_move(ecs: &SubWorld, commands: &mut CommandBuffer) {
+    let mut movers = <(Entity, &Point, &MoveRandomly)>::query();
 
-    movers.iter_mut(ecs).for_each(|(pos, _)| {
+    movers.iter(ecs).for_each(|(entity, pos, _)| {
         let mut rng = RandomNumberGenerator::new();
 
         let destination = match rng.range(0, 4) {
@@ -16,8 +16,12 @@ pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
             _ => Point::new(0, 1),
         } + *pos;
 
-        if map.can_enter_tile(destination) {
-            *pos = destination;
-        }
-    })
+        commands.push((
+            (),
+            WantsToMove {
+                entity: *entity,
+                destination,
+            },
+        ));
+    });
 }
